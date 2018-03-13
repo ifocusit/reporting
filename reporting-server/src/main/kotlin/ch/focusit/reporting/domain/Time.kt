149c3e0dfd0d9637e35f8cd82eac9c165e9c750f
jboz <julienboz@gmail.com>
2018-03-13 08:01:06 +0100

    backend spring/kotlin/mongo

--- reporting-server/src/main/kotlin/ch/focusit/reporting/domain/Time.kt
+++ reporting-server/src/main/kotlin/ch/focusit/reporting/domain/Time.kt
@@ -1,6 +1,8 @@
 package ch.focusit.reporting.domain
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 
-//@Document(collection = "times")
-class Time(var date: LocalDateTime)
+@Document(collection = "times")
+class Time(@Id val handle: String, var date: LocalDateTime)
