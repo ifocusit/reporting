e501a801190ad75c01d46ed0ccf12e2b2019b3ac
jboz <julienboz@gmail.com>
2018-05-18 09:24:45 +0200

    correct backend + test pas fini :(

--- reporting-server/src/main/kotlin/ch/focusit/reporting/domain/Time.kt
+++ reporting-server/src/main/kotlin/ch/focusit/reporting/domain/Time.kt
@@ -2,6 +2,7 @@ package ch.focusit.reporting.domain
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
 
@@ -9,4 +10,9 @@ import java.util.*
  * Timbrage
  */
 @Document(collection = "times")
-data class Time(@Id val id: String? = UUID.randomUUID().toString(), var time: LocalDateTime)
+data class Time(@Id public val id: String? = UUID.randomUUID().toString(), public var time: LocalDateTime) {
+    fun getDate(): LocalDate {
+        return time.toLocalDate()
+    }
+}
+
