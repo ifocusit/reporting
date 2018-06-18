128b2fe1598f2df457b9f3721088f876a8a5df5a
jboz <julienboz@gmail.com>
2018-06-18 20:18:00 +0200

    test

--- timbrages/src/app/app.component.ts
+++ timbrages/src/app/app.component.ts
@@ -1,19 +1,26 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs/internal/Observable";
 import {bindCallback} from "rxjs/internal/observable/bindCallback";
-import {Geolocation} from "@capacitor/core";
+import {CameraResultType, FilesystemDirectory, Geolocation, GetUriResult, Plugins} from "@capacitor/core";
 import {map} from "rxjs/operators";
-import {GeolocationPosition} from "@capacitor/core/dist/esm/core-plugin-definitions";
+import {CameraOptions, GeolocationPosition} from "@capacitor/core/dist/esm/core-plugin-definitions";
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent implements OnInit {
   title = 'Timbrage Application';
 
   coords = null;
+  image: SafeResourceUrl;
+  path: GetUriResult;
+
+  constructor(private sanitizer: DomSanitizer) {
+
+  }
 
   ngOnInit() {
     this.watchPosition().subscribe(coords => this.coords = coords);
@@ -24,5 +31,76 @@ export class AppComponent implements OnInit {
       map(position => Array.isArray(position) ? position[0] : position),
       map((position: GeolocationPosition) => position.coords)
     );
+  }
+
+  share(): void {
+    const {Share} = Plugins;
+
+    Share.share({
+      title: 'See cool stuff',
+      text: 'Really awesome thing you need to see right meow',
+      url: this.path.uri,
+      dialogTitle: 'Share with buddies'
+    })
+      .then(() => console.log('Successful share'))
+      .catch((error) => console.log('Error sharing', error));
+  }
+
+  grabPhoto() {
+
+    const {Camera, Filesystem} = Plugins;
+
+    Camera.getPhoto(<CameraOptions>{
+      quality: 100,
+      resultType: CameraResultType.Uri
+    })
+      .then((photo) => {
+          Filesystem.readFile({
+            path: photo.path
+          })
+            .then((result) => {
+              let date = new Date(),
+                time = date.getTime(),
+                fileName = time + '.jpeg';
+
+              Filesystem.writeFile({
+              ***REMOVED*** result.data,
+                path: fileName,
+                directory: FilesystemDirectory.Documents
+              })
+                .then((result) => {
+                  Filesystem.getUri({
+                    directory: FilesystemDirectory.Documents,
+                    path: fileName
+                  })
+                    .then((result) => {
+                      this.path = result;
+                    }, (err) => {
+                      console.log(err);
+                    });
+                }, (err) => {
+                  console.log(err);
+                });
+            }, (err) => {
+              console.log(err);
+            });
+        }, (err) => {
+          console.log(err);
+        }
+      );
+  }
+
+
+  takePhoto() {
+    const {Camera} = Plugins;
+    Camera.getPhoto(<CameraOptions>{
+      quality: 100,
+      resultType: CameraResultType.Base64
+    })
+      .then(photo => this.image = this.sanitizer.bypassSecurityTrustResourceUrl(photo && (photo.webPath)));
+  }
+
+  get imageSrc(): string {
+    return this.path.uri.replace('file://', '_capacitor_')
   }
 }
