1c4b09839e0940dea5881a4e3d1d4ef7ab7a88de
jboz <julienboz@gmail.com>
2018-06-28 00:22:01 +0200

    try pwa on android

--- timbrage/src/app/app.component.ts
+++ timbrage/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Timbrage';
+
+  currentLat: any;
+  currentLong: any;
+
+  ngOnInit(): void {
+    this.findMe();
+  }
+
+  findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.currentLat = position.coords.latitude;
+        this.currentLong = position.coords.longitude;
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
 }
