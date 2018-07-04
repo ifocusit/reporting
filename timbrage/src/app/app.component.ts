9aa6e52d7f05b2d6d8fbfb06c2d21f9e049e0b69
jboz <julienboz@gmail.com>
2018-07-05 00:13:07 +0200

    styling timbrage page

--- timbrage/src/app/app.component.ts
+++ timbrage/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 import {MediaMatcher} from "@angular/cdk/layout";
+import {registerLocaleData} from "@angular/common";
+import localeFr from '@angular/common/locales/fr';
+import localeFrExtra from '@angular/common/locales/extra/fr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnDestroy {
   mobileQuery: MediaQueryList;
@@ -16,6 +19,9 @@ export class AppComponent implements OnDestroy {
   private _mobileQueryListener: () => void;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+
+    registerLocaleData(localeFr, 'fr', localeFrExtra);
+
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
--- timbrage/src/app/timbrage/timbrage.component.ts
+++ timbrage/src/app/timbrage/timbrage.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-timbrage',
   templateUrl: './timbrage.component.html',
-  styleUrls: ['./timbrage.component.css']
+  styleUrls: ['./timbrage.component.scss']
 })
-export class TimbrageComponent implements OnInit {
+export class TimbrageComponent implements OnInit, OnDestroy {
+  now = new Date();
+  timerDay: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.timerDay = setInterval(() => {
+      this.now = new Date();
+    }, 1000);
   }
 
+  ngOnDestroy(): void {
+    if (this.timerDay) clearInterval(this.timerDay);
+  }
+
+  get sumDay() {
+    return 0;
+  }
 }
