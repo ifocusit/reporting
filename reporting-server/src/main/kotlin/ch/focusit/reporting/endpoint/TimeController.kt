b783d86c8572627d2cc695145d8a1c9d797411d9
jboz <julienboz@gmail.com>
2018-09-15 14:49:51 +0200

    ngrx

--- reporting-server/src/main/kotlin/ch/focusit/reporting/endpoint/TimeController.kt
+++ reporting-server/src/main/kotlin/ch/focusit/reporting/endpoint/TimeController.kt
@@ -6,15 +6,30 @@ import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.core.publisher.toFlux
+import reactor.core.publisher.toMono
 import java.time.LocalDate
+import java.time.Year
 import java.time.YearMonth
+import java.time.temporal.Temporal
+import java.util.regex.Pattern
 import javax.validation.Valid
 
 @RestController
-@CrossOrigin(value = "*")
+@CrossOrigin("*")
 @RequestMapping("times")
 class TimeController(val repository: TimeRepository) {
+
+    companion object {
+        @JvmStatic
+        val PATTERN_DAY = Pattern.compile("(\\d{4}-\\d{2}-\\d{2})")!!
+        @JvmStatic
+        val PATTERN_MONTH = Pattern.compile("(\\d{4}-\\d{2})")!!
+        @JvmStatic
+        val PATTERN_YEAR = Pattern.compile("(\\d{4})")!!
+    }
 
     @GetMapping
     fun findAll() = repository.findAllByOrderByTimeAsc()
@@ -24,34 +39,58 @@ class TimeController(val repository: TimeRepository) {
     fun insert(@RequestBody time: Time) = repository.save(time)
 
     @GetMapping("{id}")
-    fun find(@PathVariable(value = "id") id: String) =
+    fun find(@PathVariable("id") id: String) =
             repository.findById(id)
                     .map { time: Time -> ResponseEntity.ok(time) }
                     .defaultIfEmpty(ResponseEntity(HttpStatus.NOT_FOUND))
 
     @PutMapping("{id}")
-    fun update(@PathVariable(value = "id") id: String, @Valid @RequestBody updateTime: Time) =
+    fun update(@PathVariable("id") id: String, @Valid @RequestBody updateTime: Time) =
             repository.findById(id)
                     .flatMap { existingTime -> repository.save(existingTime.copy(time = updateTime.time)) }
                     .map { updatedTime: Time -> ResponseEntity.ok(updatedTime) }
                     .defaultIfEmpty(ResponseEntity(HttpStatus.NOT_FOUND))
 
     @DeleteMapping("{id}")
-    fun delete(@PathVariable(value = "id") id: String) =
+    fun delete(@PathVariable("id") id: String) =
             repository.findById(id)
-                    .flatMap { existingTime ->
-                        repository.delete(existingTime)
-                                .then(Mono.just(ResponseEntity.ok(null)))
-                    }
+                    .flatMap { existingTime -> repository.delete(existingTime).then(Mono.just(ResponseEntity.ok(null))) }
                     .defaultIfEmpty(ResponseEntity(HttpStatus.NOT_FOUND))
 
     @GetMapping("month/{month}")
-    fun getByMonth(@PathVariable(value = "month") month: YearMonth) =
-            repository.findAllByTimeBetweenOrderByTimeAsc(month.atDay(1).atStartOfDay(),
-                    month.atEndOfMonth().plusDays(1).atStartOfDay())
+    fun getByDate(@PathVariable("month") month: YearMonth) =
+            repository.findAllByTimeBetweenOrderByTimeAsc(month.atDay(1).atStartOfDay(), month.atEndOfMonth().plusDays(1).atStartOfDay())
+
+    @GetMapping("year/{year}")
+    fun getByDate(@PathVariable("year") year: Year) =
+            repository.findAllByTimeBetweenOrderByTimeAsc(year.atDay(1).atStartOfDay(), year.atDay(1).plusYears(1).atStartOfDay())
+
+    @GetMapping("day/{day}")
+    fun getByDate(@PathVariable("day") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) day: LocalDate) =
+            repository.findAllByTimeBetweenOrderByTimeAsc(day.atStartOfDay(), day.plusDays(1).atStartOfDay())
 
     @GetMapping("date/{date}")
-    fun getByMonth(@PathVariable(value = "date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) date: LocalDate) =
-            repository.findAllByTimeBetweenOrderByTimeAsc(date.withDayOfMonth(1).atStartOfDay(),
-                    date.withDayOfMonth(1).plusMonths(1).atStartOfDay())
+    fun getByDate(@PathVariable("date") date: String) =
+            readDay(date)
+                    .switchIfEmpty(readMonth(date))
+                    .switchIfEmpty(readYear(date))
+                    .flatMap { temporal ->
+                        when (temporal) {
+                            is LocalDate -> getByDate(temporal)
+                            is YearMonth -> getByDate(temporal)
+                            else -> getByDate(temporal as Year)
+                        }
+                    }
+
+    private fun readDay(date: String): Flux<Temporal> = PATTERN_DAY.matcher(date).toMono().toFlux()
+            .filter { matches -> matches.find() }
+            .map { matches -> LocalDate.parse(matches.group()) }
+
+    private fun readMonth(date: String): Flux<Temporal> = PATTERN_MONTH.matcher(date).toMono().toFlux()
+            .filter { matches -> matches.find() }
+            .map { matches -> YearMonth.parse(matches.group()) }
+
+    private fun readYear(date: String): Flux<Temporal> = PATTERN_YEAR.matcher(date).toMono().toFlux()
+            .filter { matches -> matches.find() }
+            .map { matches -> Year.parse(matches.group()) }
 }
--- timbrage/src/app/store/time/time.effects.ts
+++ timbrage/src/app/store/time/time.effects.ts
@@ -1,31 +1,26 @@
-import {environment} from '../../../environments/environment';
 import {Injectable} from '@angular/core';
 import {Action} from '@ngrx/store';
-import {Actions, Effect} from '@ngrx/effects';
+import {Actions, Effect, ofType} from '@ngrx/effects';
 
-import * as TimeActions from './time.action';
-
-import {HttpClient} from '@angular/common/http';
 import {Observable} from "rxjs/internal/Observable";
-import {TimeState} from "./time.state";
 import {catchError, map, mergeMap} from "rxjs/operators";
+import {TimesClientService} from "../../services/times-client.service";
+import {GetTimes, GetTimesError, GetTimesSuccess, TimeActionTypes} from "./time.action";
+import {Time} from "../../models/time.model";
 import {of} from "rxjs/internal/observable/of";
 
 @Injectable()
 export class TimeEffects {
 
-  constructor(private http: HttpClient, private actions$: Actions) {
-  }
+    constructor(private timesClient: TimesClientService, private actions$: Actions) {
+    }
 
-  @Effect()
-  GetTimes$: Observable<Action> = this.actions$.ofType<TimeActions.GetTimes>(TimeActions.GET_TIMES)
-    .pipe(
-      mergeMap(() =>
-        this.http.get<TimeState[]>(environment.client.base_url + '/times')
-          .pipe(
-            map((times: TimeState[]) => new TimeActions.GetTimesSuccess(times)),
-            catchError(() => of(new TimeActions.GetTimesError()))
-          )
-      )
-    );
+    @Effect()
+    GetTimes$: Observable<Action> = this.actions$
+        .pipe(
+            ofType<GetTimes>(TimeActionTypes.GET_TIMES),
+            mergeMap(action => this.timesClient.getTimes(action.date)),
+            map((times: Time[]) => new GetTimesSuccess({times: times})),
+            catchError(() => of(new GetTimesError()))
+        );
 }
