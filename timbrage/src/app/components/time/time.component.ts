d149f485b50c54959edd812a4205040659fd52a0
jboz <julienboz@gmail.com>
2018-09-24 21:39:02 +0200

    ngxs ok !

--- timbrage/src/app/components/time/time.component.ts
+++ timbrage/src/app/components/time/time.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Time, TimeAdapter} from "../../models/time.model";
+import {Store} from '@ngxs/store';
+import {Component, Input, OnInit} from "@angular/core";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Time} from "../../models/time.model";
+import {UpdateTime} from "../../store/time.store";
 
 @Component({
     selector: 'app-time',
@@ -9,7 +11,11 @@ import {Time} from "../../models/time.model";
 })
 export class TimeComponent implements OnInit {
 
-    @Input() public time: Time;
+    @Input() set time(time: Time) {
+        this.adapter = new TimeAdapter(time);
+    }
+
+    public adapter: TimeAdapter;
 
     public hours = [];
     public minutes = [];
@@ -18,7 +24,7 @@ export class TimeComponent implements OnInit {
 
     editing = false;
 
-    constructor(private fb: FormBuilder) {
+    constructor(private fb: FormBuilder, private store: Store) {
         for (let i = 0; i < 24; i++) {
             this.hours.push(i);
         }
@@ -40,13 +46,14 @@ export class TimeComponent implements OnInit {
 
     public startEditing() {
         this.editing = true;
-        this.form.setValue({hours: this.time.hours, minutes: this.time.minutes});
+        this.form.setValue({hours: this.adapter.hours, minutes: this.adapter.minutes});
     }
 
     public onSubmit() {
-        this.time.hours = this.form.value.hours;
-        this.time.minutes = this.form.value.minutes;
+        this.adapter.hours = this.form.value.hours;
+        this.adapter.minutes = this.form.value.minutes;
         this.editing = false;
+        this.store.dispatch(new UpdateTime(this.adapter.time));
     }
 
     public revert() {
--- timbrage/src/app/services/times-client.service.ts
+++ timbrage/src/app/services/times-client.service.ts
@@ -1,8 +1,8 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/internal/Observable";
 import {Time} from "../models/time.model";
 import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
     providedIn: 'root'
@@ -12,7 +12,19 @@ export class TimesClientService {
     constructor(private http: HttpClient) {
     }
 
-    public getTimes(date: string): Observable<Time[]> {
+    public read(date: string): Observable<Time[]> {
         return this.http.get<Time[]>(`${environment.client.base_url}/times/date/${date}`)
+    }
+
+    public create(time: Time): Observable<Time> {
+        return this.http.post<Time>(`${environment.client.base_url}/times`, time);
+    }
+
+    public update(time: Time): Observable<Time> {
+        return this.http.put<Time>(`${environment.client.base_url}/times/${time.id}`, time);
+    }
+
+    public delete(time: Time): Observable<Time> {
+        return this.http.delete<Time>(`${environment.client.base_url}/times/${time.id}`);
     }
 }
