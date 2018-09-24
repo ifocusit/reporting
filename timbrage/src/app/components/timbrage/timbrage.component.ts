5ab08cb16e1d0eef55ab3a75e294e156a907377e
jboz <julienboz@gmail.com>
2018-09-24 23:45:32 +0200

    header timbrages

--- timbrage/src/app/components/timbrage/timbrage.component.ts
+++ timbrage/src/app/components/timbrage/timbrage.component.ts
@@ -16,7 +16,7 @@ import {withLatestFrom} from "rxjs/operators";
 })
 export class TimbrageComponent implements OnInit, OnDestroy {
     now = new Date();
-    timerDay: any;
+    // timerDay: any;
 
     @Select(TimesState.times) times$: Observable<Time[]>;
     @Select(TimesState.loading) loading$: Observable<boolean>;
@@ -37,24 +37,10 @@ export class TimbrageComponent implements OnInit, OnDestroy {
         //   this.now = new Date();
         //   this.calculationService.calculate(this.times).then((duration) => this.sumDay = duration);
         // }, 1000);
-
-        // this.timerWeek = setInterval(() => {
-        //   this.storageService.find(moment().startOf('week'), moment().endOf('week')).then((timbrages) => {
-        //     this.calculationService.calculate(timbrages).then((duration) => this.sumWeek = duration);
-        //   });
-        // }, 1000);
-
-        // this.timerMonth = setInterval(() => {
-        //   this.storageService.find(moment().startOf('month'), moment().endOf('month')).then((timbrages) => {
-        //     this.calculationService.calculate(timbrages).then((duration) => this.sumMonth = duration);
-        //   });
-        // }, 1000);
     }
 
     private stopTimers() {
         // if (this.timerDay) clearInterval(this.timerDay);
-        // if (this.timerWeek) clearInterval(this.timerWeek);
-        // if (this.timerMonth) clearInterval(this.timerMonth);
     }
 
     ngOnDestroy(): void {
