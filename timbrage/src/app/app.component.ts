02e188270634bc95c8c09e7b0d87b164066163c6
jboz <julienboz@gmail.com>
2018-10-29 23:49:54 +0100

    export to header (not yet visible)

--- timbrage/src/app/app.component.ts
+++ timbrage/src/app/app.component.ts
@@ -1,33 +1,48 @@
-import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import {MediaMatcher} from "@angular/cdk/layout";
 import {registerLocaleData} from "@angular/common";
 import localeFr from '@angular/common/locales/fr';
 import localeFrExtra from '@angular/common/locales/extra/fr';
+import {ExportService} from "./services/export.service";
+import {TimesState} from "./store/time.store";
+import {Store} from "@ngxs/store";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnDestroy {
-  mobileQuery: MediaQueryList;
+    mobileQuery: MediaQueryList;
 
-  navLinks = [
-    {path: 'timbrage', label: 'Timbrage'},
-    {path: 'calendar', label: 'Calendar'}
-  ];
-  private _mobileQueryListener: () => void;
+    navLinks = [
+        {path: 'timbrage', label: 'Timbrage'},
+        {path: 'calendar', label: 'Calendar'}
+    ];
+    private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    @ViewChild('export') private exportLink: ElementRef;
 
-    registerLocaleData(localeFr, 'fr', localeFrExtra);
+    constructor(private changeDetectorRef: ChangeDetectorRef, private media: MediaMatcher,
+                private store: Store, private exportService: ExportService) {
 
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
+        registerLocaleData(localeFr, 'fr', localeFrExtra);
 
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
+        this.mobileQuery = media.matchMedia('(max-width: 600px)');
+        this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+        this.mobileQuery.addListener(this._mobileQueryListener);
+    }
+
+    ngOnDestroy(): void {
+        this.mobileQuery.removeListener(this._mobileQueryListener);
+    }
+
+    public exportMonth() {
+        const date = this.store.selectSnapshot(TimesState.date);
+        this.exportService.exportMonth(date, this.exportLink);
+    }
+
+    public showExport() {
+        return true;
+    }
 }
