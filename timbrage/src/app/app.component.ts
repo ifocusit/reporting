7154f6004573f9f042d659bd425e27028e6e8231
jboz <julienboz@gmail.com>
2018-11-06 23:13:36 +0100

    clean style

--- timbrage/src/app/app.component.ts
+++ timbrage/src/app/app.component.ts
@@ -1,15 +1,10 @@
-import {ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MediaMatcher} from "@angular/cdk/layout";
 import {registerLocaleData} from "@angular/common";
 import localeFr from '@angular/common/locales/fr';
 import localeFrExtra from '@angular/common/locales/extra/fr';
-import {ExportService} from "./services/export.service";
-import {AddTime, DeleteTimes, TimesState} from "./store/time.store";
-import {Store} from "@ngxs/store";
 import * as moment from "moment";
-import {LoadSettings, SetExportFormat} from "./store/settings.store";
-import {FormControl, Validators} from "@angular/forms";
-import {DATE_ISO_FORMAT, Time, TimeAdapter} from "./models/time.model";
+import {DATE_ISO_FORMAT, TimeAdapter} from "./models/time.model";
 import {TimesClientService} from "./services/times-client.service";
 import {from, of} from "rxjs/index";
 import {catchError, filter, map, mergeMap, tap} from "rxjs/operators";
@@ -19,34 +14,16 @@ import {catchError, filter, map, mergeMap, tap} from "rxjs/operators";
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
     mobileQuery: MediaQueryList;
 
-    navLinks = [
-        {path: 'timbrage', label: 'Timbrage'},
-        {path: 'calendar', label: 'Calendar'}
-    ];
-    private _mobileQueryListener: () => void;
-
-    @ViewChild('export') private exportLink: ElementRef;
-    @ViewChild('fileSelector') private fileSelector: ElementRef;
-
-    public formatFormControl = new FormControl('', [Validators.required]);
-    public times: Time[];
-
-    constructor(private changeDetectorRef: ChangeDetectorRef, private media: MediaMatcher,
-                private store: Store, private exportService: ExportService, private timeClient: TimesClientService) {
+    constructor(private media: MediaMatcher, private timeClient: TimesClientService) {
 
         registerLocaleData(localeFr, 'fr', localeFrExtra);
         moment.locale('fr');
-
-        this.mobileQuery = media.matchMedia('(max-width: 600px)');
-        this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-        this.mobileQuery.addListener(this._mobileQueryListener);
     }
 
     ngOnInit(): void {
-        this.store.dispatch(new LoadSettings()).subscribe(state => this.formatFormControl.setValue(state.settings.exportFormat));
         setTimeout(() => this.migrateStorage(), 1000);
     }
 
@@ -64,46 +41,5 @@ export class AppComponent implements OnInit, OnDestroy {
             tap(times => localStorage.removeItem(new TimeAdapter(times[0]).format(DATE_ISO_FORMAT))),
             catchError(() => of(true))
         ).subscribe().unsubscribe();
-    }
-
-    ngOnDestroy(): void {
-        this.mobileQuery.removeListener(this._mobileQueryListener);
-    }
-
-    public exportMonth() {
-        const date = this.store.selectSnapshot(TimesState.date);
-        this.exportService.exportMonth(date, this.exportLink);
-    }
-
-    public calendarPage() {
-        return window.location.pathname.match('.*calendar.*');
-    }
-
-    public setExportFormat() {
-        this.store.dispatch(new SetExportFormat(this.formatFormControl.value));
-    }
-
-    public selectFile() {
-        this.fileSelector.nativeElement.click();
-    }
-
-    public fileChanged(e) {
-        const file = e.target.files[0];
-        let reader = new FileReader();
-        reader.onload = () => {
-            this.times = [];
-            reader.result.split('\r\n').forEach(line => {
-                this.times.push(TimeAdapter.createTime(line));
-            });
-            this.times = this.times.filter(time => !!time);
-            this.store.dispatch(new AddTime(this.times, true));
-        };
-        reader.readAsText(file);
-    }
-
-    public cancelImport() {
-        if (this.times) {
-            this.store.dispatch(new DeleteTimes(this.times)).subscribe(() => this.times = undefined);
-        }
     }
 }
---
+++ timbrage/src/app/components/home/home.component.ts
@@ -0,0 +1,45 @@
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {TimesState} from "../../store/time.store";
+import {ExportService} from "../../services/export.service";
+import {Store} from "@ngxs/store";
+import {SelectDate} from "../../store/calendar.store";
+import * as moment from "moment";
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+    navLinks = [
+        {path: 'timbrage', label: 'Timbrage'},
+        {path: 'calendar', label: 'Calendar'}
+    ];
+
+    isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+        .pipe(
+            map(result => result.matches)
+        );
+
+    @ViewChild('export') private exportLink: ElementRef;
+
+    constructor(private breakpointOb***REMOVED*** BreakpointObserver, private exportService: ExportService, private store: Store) {
+    }
+
+    public calendarPage() {
+        return window.location.pathname.match('.*calendar.*');
+    }
+
+    public exportMonth() {
+        const date = this.store.selectSnapshot(TimesState.date);
+        this.exportService.exportMonth(date, this.exportLink);
+    }
+
+    public goToday() {
+        this.store.dispatch(new SelectDate(moment()));
+    }
+}
