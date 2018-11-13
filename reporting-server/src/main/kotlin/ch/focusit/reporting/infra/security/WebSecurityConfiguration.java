1b83364a333d73d968fd843ef7289d641c5f4466
jboz <julienboz@gmail.com>
2018-11-13 20:09:38 +0100

    catcheError if read failed

--- reporting-server/src/main/kotlin/ch/focusit/reporting/infra/security/WebSecurityConfiguration.java
+++ reporting-server/src/main/kotlin/ch/focusit/reporting/infra/security/WebSecurityConfiguration.java
@@ -1,36 +1,18 @@
 package ch.focusit.reporting.infra.security;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public FilterRegistrationBean corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .oauth2Login();
+                .anyRequest().permitAll()
+//                .and()
+//                .oauth2Login()
+        ;
     }
 }
--- timbrage/src/app/services/times-client.service.ts
+++ timbrage/src/app/services/times-client.service.ts
@@ -1,35 +1,35 @@
-import {Injectable} from "@angular/core";
-import {Observable} from "rxjs/internal/Observable";
-import {Time} from "../models/time.model";
-import {environment} from "../../environments/environment";
-import {HttpClient} from "@angular/common/http";
-import {from} from "rxjs";
-import {mergeMap, toArray} from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/internal/Observable";
+import { Time } from "../models/time.model";
+import { environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { from } from "rxjs";
+import { mergeMap, toArray } from "rxjs/operators";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class TimesClientService {
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    public read(date: string): Observable<Time[]> {
-        return this.http.get<Time[]>(`${environment.client.base_url}/times/date/${date}`)
-    }
+  public read(date: string): Observable<Time[]> {
+    return this.http.get<Time[]>(`${environment.client.base_url}/times/date/${date}`)
+  }
 
-    public create(times: Time[], uniq?): Observable<Time[]> {
-        return from(times).pipe(
-            mergeMap(time => this.http.post<Time>(`${environment.client.base_url}/times`, time)),
-            toArray()
-        );
-    }
+  public create(times: Time[], uniq?): Observable<Time[]> {
+    return from(times).pipe(
+      mergeMap(time => this.http.post<Time>(`${environment.client.base_url}/times`, time)),
+      toArray()
+    );
+  }
 
-    public update(time: Time): Observable<Time> {
-        return this.http.put<Time>(`${environment.client.base_url}/times/${time.id}`, time);
-    }
+  public update(time: Time): Observable<Time> {
+    return this.http.put<Time>(`${environment.client.base_url}/times/${time.id}`, time);
+  }
 
-    public delete(time: Time): Observable<any> {
-        return this.http.delete<any>(`${environment.client.base_url}/times/${time.id}`);
-    }
+  public delete(time: Time): Observable<any> {
+    return this.http.delete<any>(`${environment.client.base_url}/times/${time.id}`);
+  }
 }
