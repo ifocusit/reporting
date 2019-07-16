b12b11ded4d7e98ebce92dadb8a3372fd0f7cba5
Julien Boz <julienboz@gmail.com>
2019-07-16 23:48:12 +0200

    config deploy

--- timbrage/src/environments/environment.ts
+++ timbrage/src/environments/environment.ts
@@ -4,17 +4,14 @@
 
 export const environment = {
   production: false,
-  client: {
-    in_memory: true,
-  },
   firebaseConfig: {
-    apiKey: "AIzaSyC36dQ0yvYx_gqq0cSdN9D87DuSf5u-TFg",
-    authDomain: "timbrage-reporting.firebaseapp.com",
-    databaseURL: "https://timbrage-reporting.firebaseio.com",
-    projectId: "timbrage-reporting",
-    storageBucket: "timbrage-reporting.appspot.com",
-    messagingSenderId: "836360158078",
-    appId: "1:836360158078:web:6608488d5c1bc88b"
+    apiKey: 'AIzaSyC36dQ0yvYx_gqq0cSdN9D87DuSf5u-TFg',
+    authDomain: 'timbrage-reporting.firebaseapp.com',
+    databaseURL: 'https://timbrage-reporting.firebaseio.com',
+    projectId: 'timbrage-reporting',
+    storageBucket: 'timbrage-reporting.appspot.com',
+    messagingSenderId: '836360158078',
+    appId: '1:836360158078:web:6608488d5c1bc88b'
   }
 };
 
