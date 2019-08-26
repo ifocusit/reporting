f860b4bf210c91f844af59d56733ec21351a77db
Julien Boz <julienboz@gmail.com>
2019-08-26 12:04:25 +0200

    clean

--- reporting-server/src/main/kotlin/ch/focusit/reporting/repository/SettingRepository.kt
+++
@@ -1,6 +0,0 @@
-package ch.focusit.reporting.repository
-
-import ch.focusit.reporting.domain.Setting
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
-
-interface SettingRepository : ReactiveCrudRepository<Setting, String>
--- timbrage/README.md
+++
@@ -1,8 +0,0 @@
-# Timbrage
-
-## TODO
-
-- licence/copyright
-- afficher total mensuel
-- afficher total annuel => paramétrable
-- lien vers la version reporting complet
