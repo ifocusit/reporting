a9a7e8257fd21f5f07ec3c7e0d4ae632a1051325
Julien Boz <julienboz@gmail.com>
2019-08-29 02:37:02 +0200

    scroll ok !

--- projects/commons/src/lib/reporting-common.module.ts
+++ projects/commons/src/lib/reporting-common.module.ts
@@ -6,10 +6,24 @@ import { MomentPipe } from './pipes/moment.pipe';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LayoutModule } from '@angular/cdk/layout';
 import { DebounceInputDirective } from './form/debounce-input.directive';
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = {
+    pinch: { enable: false },
+    rotate: { enable: false }
+  } as any;
+}
 
 @NgModule({
   declarations: [DurationPipe, MomentPipe, DebounceInputDirective],
-  providers: [DecimalPipe],
+  providers: [
+    DecimalPipe,
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig
+    }
+  ],
   imports: [CommonModule],
   exports: [DurationPipe, MomentPipe, DebounceInputDirective, MaterialModule, FlexLayoutModule, LayoutModule]
 })
