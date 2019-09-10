54cdfe05351043b34bf8f9cad1060ff3fe519aad
Julien Boz <julienboz@gmail.com>
2019-09-11 00:07:47 +0200

    copyright

--- projects/admin/src/app/components/profile/profile.module.ts
+++ projects/admin/src/app/components/profile/profile.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
-import { ReportingCommonModule } from 'projects/commons/src/public-api';
+import { ReportingCommonModule, CopyrightModule } from 'projects/commons/src/public-api';
 import { ProfileComponent } from './profile.component';
 import { SelectProjectModule } from 'projects/commons/src/lib/settings/select-project/select-project.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -10,6 +10,15 @@ import { FilesModule } from 'projects/commons/src/lib/files/files.module';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule, ReportingCommonModule, SelectProjectModule, ReactiveFormsModule, FormsModule, FilesModule]
+  imports: [
+    CommonModule,
+    ProfileRoutingModule,
+    ReportingCommonModule,
+    SelectProjectModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FilesModule,
+    CopyrightModule
+  ]
 })
 export class ProfileModule {}
