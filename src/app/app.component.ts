e342fb78d6982e4f67e463376d6f4d9f0f1b5306
Julien Boz <julienboz@gmail.com>
2019-09-16 10:35:27 +0200

    config test && remove empty tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from 'projects/commons/src/lib/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +8,5 @@ import { AuthService } from 'projects/commons/src/lib/auth/auth.service';
 export class AppComponent {
   title = 'reporting';
 
-  public user$;
-
-  constructor(private authService: AuthService) {
-    this.user$ = this.authService.user$;
-  }
-
-  signout() {
-    this.authService.signOutUser();
-  }
+  constructor() {}
 }
