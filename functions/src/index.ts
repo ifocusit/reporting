2657a06669aa7dcda6a9e3e346a8bab568b173ae
Julien Boz <julienboz@gmail.com>
2019-09-24 23:02:24 +0200

    functions

---
+++ functions/src/index.ts
@@ -0,0 +1,36 @@
+import * as functions from 'firebase-functions';
+import { UserRecord } from 'firebase-functions/lib/providers/auth';
+import * as nodemailer from 'nodemailer';
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello World!');
+});
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: functions.config().mail.auth.user, // gmail user
+    pass: functions.config().mail.auth.pass // gmail generated password
+  }
+});
+
+const sendWelcomeEmail = function(user: UserRecord) {
+  const dest = user.email;
+  const displayName = user.displayName;
+
+  const mailOptions = {
+    from: functions.config().mail.from, // contact email adresse
+    to: dest,
+    subject: `Welcome!`,
+    html: `Welcome ${displayName}`
+  };
+
+  return transporter.sendMail(mailOptions);
+};
+
+exports.userOnCreate = functions.auth.user().onCreate((user: UserRecord) => {
+  return sendWelcomeEmail(user);
+});
