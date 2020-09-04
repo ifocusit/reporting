f2e25ea40b21aec5a9119f87c1b52cc665964ee9
Julien Boz <julienboz@gmail.com>
2020-09-04 12:24:07 +0200

    config

--- apps/reporting/jest.config.js
+++ apps/reporting/jest.config.js
@@ -1,10 +1,19 @@
 module.exports = {
+  id: 'reporting',
   name: 'reporting',
   preset: '../../jest.config.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: ['jest-preset-angular/build/InlineFilesTransformer', 'jest-preset-angular/build/StripStylesTransformer']
+    }
+  },
   coverageDirectory: '../../coverage/apps/reporting',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
 };
--- apps/timbrage/jest.config.js
+++ apps/timbrage/jest.config.js
@@ -1,10 +1,19 @@
 module.exports = {
+  id: 'timbrage',
   name: 'timbrage',
   preset: '../../jest.config.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: ['jest-preset-angular/build/InlineFilesTransformer', 'jest-preset-angular/build/StripStylesTransformer']
+    }
+  },
   coverageDirectory: '../../coverage/apps/timbrage',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
 };
