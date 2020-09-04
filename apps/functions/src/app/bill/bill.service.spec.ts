e5892b9b127fa3e16e1bd25b44fbc2f387a8da72
Julien Boz <julienboz@gmail.com>
2020-09-04 12:28:34 +0200

    correct tests

--- apps/functions/src/app/bill/bill.service.spec.ts
+++ apps/functions/src/app/bill/bill.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { SettingsService } from '../settings/settings.service';
+import { TimeService } from '../time/time.service';
 import { BillService } from './bill.service';
 
 describe('BillService', () => {
@@ -6,7 +8,7 @@ describe('BillService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BillService],
+      providers: [BillService, { provide: TimeService, useValue: jest.fn() }, { provide: SettingsService, useValue: jest.fn() }]
     }).compile();
 
     service = module.get<BillService>(BillService);
